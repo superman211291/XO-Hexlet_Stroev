44020de52f4c419618ec375fbaec8c434d89ea93
IlyaStroev <ilya_stroev@mail.ru>
2020-08-13 18:05:54 +0300

    Create FieldTest

---
+++ test/io/hexlet/xo/model/FieldTest.java
@@ -0,0 +1,16 @@
+package io.hexlet.xo.model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FieldTest {
+
+    @Test
+    public void setFigure() {
+    }
+
+    @Test
+    public void getSIZE() {
+    }
+}
