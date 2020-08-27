645efdaf22260773c94f80aa09840f0acdfe2291
IlyaStroev <ilya_stroev@mail.ru>
2020-08-27 14:35:56 +0300

    PlayerTest -- commit

--- test/io/hexlet/xo/model/PlayerTest.java
+++ test/io/hexlet/xo/model/PlayerTest.java
@@ -7,10 +7,28 @@ import static org.junit.Assert.*;
 public class PlayerTest {
 
     @Test
-    public void getName() {
+    public void testGetName() {
+        final String inputValue = "Ilya";
+
+        final String expectedValue = /*"12345"*/ inputValue;
+
+        final Player player = new Player(inputValue,null);
+
+        final String actualValue = player.getName();
+
+        assertEquals(expectedValue,actualValue);
     }
 
     @Test
-    public void getFigure() {
+    public void testGetFigure() {
+        final Figure inputValue = Figure.X;
+
+        final Figure expectedValue = /*Figure.O*/ inputValue;
+
+        final Player player = new Player(null,inputValue);
+
+        final Figure actualValue = player.getFigure();
+
+        assertEquals(expectedValue,actualValue);
     }
 }
