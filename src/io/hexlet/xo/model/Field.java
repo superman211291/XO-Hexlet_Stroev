a02bbcbbdcd7aab24053de87ef4267dc01a47fbf
IlyaStroev <ilya_stroev@mail.ru>
2020-08-27 14:22:57 +0300

    Field -- commit

--- src/io/hexlet/xo/model/Field.java
+++ src/io/hexlet/xo/model/Field.java
@@ -1,4 +1,23 @@
 package io.hexlet.xo.model;
 
+import java.awt.*;
+
 public class Field {
+
+    private final int SIZE = 3;
+
+    private final Figure[][] field = new Figure[SIZE][SIZE];
+
+    public void setFigure(final Point point, final Figure figure) {
+        field[point.x][point.y] = figure;
+    }
+
+    public int getSIZE() {
+        return SIZE;
+    }
+
+    public Figure getFigure(final Point point) {
+        return field[point.x][point.y];
+    }
+
 }
