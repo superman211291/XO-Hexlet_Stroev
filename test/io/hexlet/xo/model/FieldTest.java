83eb488460eb5d9321cef6e1d059697f8f0e72d1
IlyaStroev <ilya_stroev@mail.ru>
2020-08-27 14:23:11 +0300

    FieldTest -- commit

--- test/io/hexlet/xo/model/FieldTest.java
+++ test/io/hexlet/xo/model/FieldTest.java
@@ -2,15 +2,28 @@ package io.hexlet.xo.model;
 
 import org.junit.Test;
 
+import java.awt.*;
+
 import static org.junit.Assert.*;
 
 public class FieldTest {
 
     @Test
-    public void setFigure() {
+    public void testSetFigure() {
+        final Field field = new Field();
+        final Point point = new Point(0,0);
+        final Figure inputFigure = Figure.X;
+
+        field.setFigure(point,inputFigure);
+        final Figure actualFigure = field.getFigure(point);
+
+        assertEquals(inputFigure,actualFigure);
     }
 
     @Test
-    public void getSIZE() {
+    public void testGetSIZE() {
+        final Field field = new Field();
+
+        assertEquals(3,field.getSIZE());
     }
 }
